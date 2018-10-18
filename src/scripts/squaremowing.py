c827b6983b8e0866a8befb17590be37a3d8ba5d2
robproj <robproj@robproj-3>
2018-10-18 15:41:30 +0200

    Adding variable angle to turning and to distance to drive into the square

--- src/scripts/squaremowing.py
+++ src/scripts/squaremowing.py
@@ -1,76 +1,19 @@
 #!/usr/bin/env python
 
 import math
+import signal
+import sys
+import tf
+
 import rospy
-from geometry_msgs.msg import Twist, Pose
+from geometry_msgs.msg import Pose, Twist
 from nav_msgs.msg import Odometry
 from std_msgs.msg import String
 #sudo chmod 666 /dev/ttyACM0
 
-def forward(vel_msg, velocity_publisher, speed, distance):
-    vel_msg.linear.y = 0
-    vel_msg.linear.z = 0
-    vel_msg.angular.x = 0
-    vel_msg.angular.y = 0
-    vel_msg.angular.z = 0
-
-    # Setting the current calculus
-    t0 = rospy.Time.now().to_sec()
-    current_distance = 0
-    vel_msg.linear.x = abs(speed)
-
-    # Loop to move the turtle in an specified distance
-    while (current_distance < distance):
-        # Publish the velocity
-        velocity_publisher.publish(vel_msg)
-        # Takes actual time to velocity calculus
-        t1=rospy.Time.now().to_sec()
-        # Calculates distancePoseStamped
-        current_distance= speed*(t1-t0)
-    
-    # After the loop, stops the robot.
-    vel_msg.linear.x = 0
-
-    velocity_publisher.publish(vel_msg)
-
-    return True
-
-def turn(vel_msg, velocity_publisher):
-    angular_speed = 1
-    relative_angle = math.pi
-
-    #We wont use linear components
-    vel_msg.linear.x=0
-    vel_msg.linear.y=0
-    vel_msg.linear.z=0
-    vel_msg.angular.x = 0
-    vel_msg.angular.y = 0
-
-    # # Checking if our movement is CW or CCW
-    # if clockwise:
-    #     vel_msg.angular.z = -abs(angular_speed)
-    # else:
-    #     vel_msg.angular.z = abs(angular_speed)
-    
-    vel_msg.angular.z = -abs(angular_speed)
-    
-    # Setting the current time for distance calculus
-    t0 = rospy.Time.now().to_sec()
-    current_angle = 0
-
-    while(current_angle < relative_angle):
-        velocity_publisher.publish(vel_msg)
-        t1 = rospy.Time.now().to_sec()
-        current_angle = angular_speed*(t1-t0)
-
-    #Forcing our robot to stop
-    vel_msg.angular.z = 0
-    velocity_publisher.publish(vel_msg)
-    #rospy.spin()
-
-    return True   
-
-
+def signal_handler(sig, frame):
+        print('Exit')
+        sys.exit(0)
 
 class HRQ(object):
     
@@ -101,14 +44,20 @@ class HRQ(object):
     def update(self, data):
         self.x = data.pose.pose.position.x
         self.y = data.pose.pose.position.y
+        
+        # Orientation in quaternions
+        self.qx = data.pose.pose.orientation.x
+        self.qy = data.pose.pose.orientation.y
+        self.qz = data.pose.pose.orientation.z
+        self.qw = data.pose.pose.orientation.w
 
     def is_within_square(self):
-        print("CHECKING IF I AM WITHIN SQUARE")
-        print("X: %s, Y: %s" % (str(self.x), str(self.y)))
-        print("ABS X: %s, ABS Y: %s" % (str(abs(self.x)), str(abs(self.y))))
-        print("SQSIZE_X: %s, SQSIZE_Y: %s" % (str(self.square_size_x/2), str(self.square_size_y/2)))
-        print()
-        rospy.loginfo(self.x)
+        # print("CHECKING IF I AM WITHIN SQUARE")
+        # print("X: %s, Y: %s" % (str(self.x), str(self.y)))
+        # print("ABS X: %s, ABS Y: %s" % (str(abs(self.x)), str(abs(self.y))))
+        # print("SQSIZE_X: %s, SQSIZE_Y: %s" % (str(self.square_size_x/2), str(self.square_size_y/2)))
+        # print()
+        # rospy.loginfo(self.x)
 
         if (abs(self.x) <= (self.square_size_x/2) and abs(self.y) <= (self.square_size_y/2)):
             return True
@@ -151,8 +100,10 @@ class HRQ(object):
         # Setting the current time for distance calculus
         t0 = rospy.Time.now().to_sec()
         current_angle = 0
+        err_angle = abs(self.turning_angle())
+        
 
-        while(current_angle < relative_angle):
+        while(current_angle < err_angle):
             self.velocity_publisher.publish(self.vel_msg)
             t1 = rospy.Time.now().to_sec()
             current_angle = self.angular_speed*(t1-t0)
@@ -162,8 +113,17 @@ class HRQ(object):
         current_distance = 0
         self.vel_msg.linear.x = abs(speed)
 
+        # if (abs(self.x) > square_size_x/2):
+        #     print("--- out of boundarie in x ---")
+        #     err_dist = math.sqrt(math.pow(square_size_x/2 - abs(self.x), 2) + math.pow(square_size_y/2 - abs(self.y), 2))
+        # elif (abs(self.y) > square_size_y/2):
+        #     print("--- out of boundarie in y ---")
+        #     err_dist = square_size_y/2 - abs(self.y)
+
+
+
         # Loop to move the turtle in an specified distance
-        while (current_distance < 0.2):
+        while (current_distance < 0.5):
             # Publish the velocity
             self.velocity_publisher.publish(self.vel_msg)
             # Takes actual time to velocity calculus
@@ -183,6 +143,27 @@ class HRQ(object):
 
         return True
 
+
+
+    def turning_angle(self):
+        # Finds the angle needed to turn back to the square
+        quaternion = (
+        self.qx,
+        self.qy,
+        self.qz,
+        self.qw)
+
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+
+        ori_angle = euler[2]
+        offset_angle = math.atan2(self.y , self.x)  # .atn(y,x) takes the signs into account
+        print("ori_angle: %s, offset_angle: %s" % (str(ori_angle), str(offset_angle)))
+        
+        output = PI - ori_angle - offset_angle
+        print("output is: %s" % str(output))
+        return output
+
+
     def main(self):
 
         while(True):
@@ -192,12 +173,13 @@ class HRQ(object):
 
 if __name__ == '__main__':
     try:
-        # Receiveing the user's input
+        signal.signal(signal.SIGINT, signal_handler)
+        # Receiveing the user's iself.xnput
         print("Let's move your robot")
         speed = 0.5 #input("Input your speed(m/s):")
         print("The robot will start in the middle of the area")
-        square_size_x = 2 # input("Input the length of the area: ")
-        square_size_y = 2 # input("Input the width of the area: ")
+        square_size_x = 1.5 # input("Input the length of the area: ")
+        square_size_y = 1.5 # input("Input the width of the area: ")
 
         # How the robot will turn on the edges
         anglespeed = 50 # input("Input your speed (degrees/sec): ")
@@ -214,5 +196,3 @@ if __name__ == '__main__':
         hrq.main()
 
     except rospy.ROSInterruptException: pass
-
-
